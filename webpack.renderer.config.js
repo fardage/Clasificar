336e3eb17d50c060b213f86044dca20ee2ddd299
Marvin Tseng <marvin@tseng.ch>
2021-08-03 15:12:37 +0200

    feat: :lipstick: logo added

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,10 +1,24 @@
 const webpack = require("webpack");
 const rules = require("./webpack.rules");
 
-rules.push({
-  test: /\.css$/,
-  use: [{ loader: "style-loader" }, { loader: "css-loader" }],
-});
+rules.push(
+  {
+    test: /\.css$/,
+    use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+  },
+  {
+    test: /\.(png|jpg|svg|jpeg|gif)$/i,
+    use: [
+      {
+        loader: "file-loader",
+        options: {
+          name: "assets/[name].[ext]",
+          publicPath: "../.",
+        },
+      },
+    ],
+  }
+);
 
 module.exports = {
   // Put your normal webpack config below here
